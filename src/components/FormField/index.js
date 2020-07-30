0d710ab6ed0d77087ca448c9bd76ce419221e4af
Petherson Moreno <pethermoreno@gmail.com>
2020-07-30 01:59:23 -0300

    refactor: updated FormField with proptypes and eslint fixes

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FormField = ({ label, type, name, value, onChange })=>{
+const FormField = ({
+  label, type, name, value, onChange,
+}) => {
+  const fieldId = `id_${name}`;
   const isTextarea = type === 'textarea';
-  const Component = isTextarea?'textarea':'input';
+  const Component = isTextarea ? 'textarea' : 'input';
   return (
     <div>
-      <label>
-        {label}:
+      <label htmlFor={fieldId}>
+        {label}
+        :
         <Component
-          type={isTextarea?undefined:type}
+          id={fieldId}
+          type={isTextarea ? undefined : type}
           name={name}
           value={value}
           onChange={onChange}
-          />
+        />
       </label>
     </div>
   );
+};
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 export default FormField;
