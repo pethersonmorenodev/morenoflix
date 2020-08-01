35d0050777a0a25e88c8a460677fd38df5e280ee
Petherson Moreno <pethermoreno@gmail.com>
2020-08-01 11:37:35 -0300

    feat: added prop paddingAll to PageDefault

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Menu from '../Menu';
 import Footer from '../Footer';
 
@@ -11,18 +11,27 @@ const Main = styled.main`
   padding-top: 50px;
   padding-left: 5%;
   padding-right: 5%;
+  ${({ paddingAll }) =>
+    paddingAll !== undefined &&
+    css`
+      padding: ${paddingAll};
+    `}
 `;
 
-const PageDefault = ({ children }) => (
+const PageDefault = ({ children, paddingAll }) => (
   <>
     <Menu />
-    <Main>{children}</Main>
+    <Main paddingAll={paddingAll}>{children}</Main>
     <Footer />
   </>
 );
 const ChildElement = PropTypes.oneOfType([PropTypes.element, PropTypes.bool, PropTypes.string]);
+PageDefault.defaultProps = {
+  paddingAll: undefined,
+};
 PageDefault.propTypes = {
   children: PropTypes.oneOfType([ChildElement, PropTypes.arrayOf(ChildElement)]).isRequired,
+  paddingAll: PropTypes.number,
 };
 
 export default PageDefault;
