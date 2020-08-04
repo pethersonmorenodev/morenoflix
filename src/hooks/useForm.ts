0ca11048fcac51a06aade9f3caf5a4087b7f548d
Petherson Moreno <pethermoreno@gmail.com>
2020-08-04 02:12:19 -0300

    feat: continued useForm using Dev Soutinho video as base
    Reference: https://www.youtube.com/watch?v=-nYNd6EuZHU&feature=youtu.be

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,27 +1,57 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
-function useForm<T>(
-  valoresIniciais: T,
-): {
-  values: T;
+type TParamsUseForm<TData, TErrors> = {
+  initialValues: TData;
+  validate?: (values: TData) => TErrors;
+};
+function useForm<TData, TErrors>({
+  initialValues,
+  validate,
+}: TParamsUseForm<TData, TErrors>): {
+  values: TData;
+  errors: TErrors;
+  touched: any;
   handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  handleBlur: (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
   clearForm: () => void;
 } {
-  const [values, setValues] = useState<T>(valoresIniciais);
-  const setValue = (key: string, value: any) => {
-    setValues(old => ({ ...old, [key]: value }));
-  };
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { value } = event.target;
-    const name = event.target.getAttribute('name');
-    if (name) {
-      setValue(name, value);
+  const [errors, setErrors] = useState({} as TErrors);
+  const [touched, setTouchedFields] = useState<any>({});
+  const [values, setValues] = useState<TData>(initialValues);
+  useEffect(() => {
+    if (validate) {
+      setErrors(validate(values));
     }
+  }, [values, validate]);
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const fieldName = event.target.getAttribute('name');
+    if (!fieldName) {
+      return;
+    }
+    const { value } = event.target;
+    setValues(oldValues => ({
+      ...oldValues,
+      [fieldName]: value,
+    }));
+  }, []);
+  const handleBlur = useCallback((event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const fieldName = event.target.getAttribute('name');
+    if (!fieldName) {
+      return;
+    }
+    setTouchedFields((oldValues: any) => ({ ...oldValues, [fieldName]: true }));
+  }, []);
+  const clearForm = useCallback(() => {
+    setValues(initialValues);
+  }, []);
+  return {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    clearForm,
   };
-  const clearForm = () => {
-    setValues(valoresIniciais);
-  };
-  return { values, handleChange, clearForm };
 }
 
 export default useForm;
