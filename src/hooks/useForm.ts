8321a5ad1524f227791a6b184772168028a12256
Petherson Moreno <pethermoreno@gmail.com>
2020-08-04 02:53:59 -0300

    feat: added invalid to useForm hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -11,16 +11,21 @@ function useForm<TData, TErrors>({
   values: TData;
   errors: TErrors;
   touched: any;
+  invalid: boolean;
   handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
   handleBlur: (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
   clearForm: () => void;
 } {
   const [errors, setErrors] = useState({} as TErrors);
   const [touched, setTouchedFields] = useState<any>({});
+  const [invalid, setInvalid] = useState(true);
   const [values, setValues] = useState<TData>(initialValues);
   useEffect(() => {
     if (validate) {
-      setErrors(validate(values));
+      const newErrors = validate(values);
+      const newInvalid = !!Object.keys(newErrors).find(k => !!(newErrors as any)[k]);
+      setErrors(newErrors);
+      setInvalid(newInvalid);
     }
   }, [values, validate]);
   const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -43,11 +48,13 @@ function useForm<TData, TErrors>({
   }, []);
   const clearForm = useCallback(() => {
     setValues(initialValues);
-  }, []);
+    setTouchedFields({});
+  }, [initialValues]);
   return {
     values,
     errors,
     touched,
+    invalid,
     handleChange,
     handleBlur,
     clearForm,
