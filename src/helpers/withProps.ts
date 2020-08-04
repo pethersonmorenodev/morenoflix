0a8dd92190ccd385905696fbc572a615a3f301f5
Petherson Moreno <pethermoreno@gmail.com>
2020-08-03 22:48:40 -0300

    fix: fixed withProps to work on the use of tags too

--- src/helpers/withProps.ts
+++ src/helpers/withProps.ts
@@ -4,6 +4,6 @@ import { ThemedStyledFunction } from 'styled-components';
 export function withProps<TypeComponent>() {
   return <P extends keyof JSX.IntrinsicElements | React.ComponentType<any>, T extends object, O extends object = {}>(
     fn: ThemedStyledFunction<P, T, O>,
-  ): ThemedStyledFunction<React.ComponentType<TypeComponent>, T, O> =>
-    (fn as unknown) as ThemedStyledFunction<React.ComponentType<TypeComponent>, T, O>;
+  ): ThemedStyledFunction<React.ComponentType<TypeComponent>, T, O & TypeComponent> =>
+    (fn as unknown) as ThemedStyledFunction<React.ComponentType<TypeComponent>, T, O & TypeComponent>;
 }
